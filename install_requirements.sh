3d581205d992b3bf8e9e04becf82027cb43f0036
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-07-22 14:50:39 -0400

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,3 +2,4 @@
 
 #install minimum requirements
 sudo apt install apache2  php libapache2-mod-php php8.1-mbstring php-intl php-mysql -y 
+echo "To create you project: bash create_project.sh [[project name]]"
