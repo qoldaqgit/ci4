0ffdab88420236de79aa1dd9c9dc224e03b2d0f4
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-07-22 15:23:47 -0400

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,8 +3,8 @@
 #install minimum requirements
 sudo apt update && sudo apt upgrade -y
 sudo apt install apache2  php libapache2-mod-php php8.1-mbstring php-intl php-mysql -y 
-wget https://github.com/qoldaqgit/ci4/blob/main/create_project.sh
-wget https://github.com/qoldaqgit/ci4/blob/main/create_controller.sh
+wget https://raw.githubusercontent.com/qoldaqgit/ci4/main/create_project.sh
+wget https://raw.githubusercontent.com/qoldaqgit/ci4/main/create_controller.sh
 
 echo "To create your project: bash create_project.sh [[project name]]"
 echo "To create your controller: bash create_controller.sh [[project name]] [[controller name]]"
