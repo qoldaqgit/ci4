4acd74d6015cd9d8d056cd77bfc97e340d35c62d
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-07-25 13:42:58 -0400

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -6,17 +6,23 @@ sudo apt install apache2  php libapache2-mod-php php8.1-mbstring php-intl php-my
 wget https://raw.githubusercontent.com/qoldaqgit/ci4/main/create_project.sh
 wget https://raw.githubusercontent.com/qoldaqgit/ci4/main/create_controller.sh
 
-echo "To create your project: bash create_project.sh [[project name]]"
-echo "To create your controller: bash create_controller.sh [[project name]] [[controller name]]"
+
 
 clear
 
-echo "List of installed Software:\n
-Apache2 - Web Server\n
-PHP - Server Side code\n
-gh - to get Github repositories\n
-nano - Text file editor\n
-ssmtp - Email sender\n"
+echo "
+List of installed Software:
+
+Apache2 - Web Server
+PHP - Server Side code
+gh - to get Github repositories
+nano - Text file editor
+ssmtp - Email sender, run sudo nano /etc/ssmtp/ssmtp.conf to add you config
+CodeIgniter 4 - PHP Framework
+
+To create your CI4 project: bash create_project.sh [[project name]]
+To create your CI4 controller: bash create_controller.sh [[project name]] [[controller name]]
+"
 
 
 
@@ -29,6 +35,9 @@ fi
 wget -q https://raw.githubusercontent.com/qoldaqgit/ci4/main/ubuntu_ODBC18_driver.sh
 wget -q https://raw.githubusercontent.com/qoldaqgit/ci4/main/ubuntu_mssql_driver_php8.sh
 
-echo "List of AAvailable Software:\n
-ODBC18 Driver - run sudo bash ubuntu_ODBC18_driver.sh to install\n
-MSSQL Driver - run sudo bash uubuntu_mssql_driver_php8.sh to install\n"
+echo "
+List of Available Software:
+
+ODBC18 Driver - run sudo bash ubuntu_ODBC18_driver.sh to install
+MSSQL Driver - run sudo bash uubuntu_mssql_driver_php8.sh to install
+"
