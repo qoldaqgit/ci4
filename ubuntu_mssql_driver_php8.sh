f009f14fa753494331525c5040fa61d2ba9b9e4b
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-07-26 17:57:52 -0400

    Update ubuntu_mssql_driver_php8.sh

--- ubuntu_mssql_driver_php8.sh
+++ ubuntu_mssql_driver_php8.sh
@@ -1,9 +1,9 @@
 #https://learn.microsoft.com/en-us/sql/connect/php/installation-tutorial-linux-mac?view=sql-server-ver16
 #Step 1. Install PHP
-#sudo su
-#add-apt-repository ppa:ondrej/php -y
-#apt-get update
-#apt-get install php8.1 php8.1-dev php8.1-xml -y --allow-unauthenticated
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+sudo apt-get install php8.1 php8.1-dev php8.1-xml -y --allow-unauthenticated
+sudo apt install php8.1-mbstring php-intl php-mysql nano ssmtp -y 
 
 #Step 2. Install prerequisites
 sudo apt-get install unixodbc-dev php-pear
@@ -12,19 +12,15 @@ pecl channel-update pecl.php.net
 #Step 3. Install the PHP drivers for Microsoft SQL Server 
 sudo pecl install sqlsrv
 sudo pecl install pdo_sqlsrv
-sudo su
-printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.1/mods-available/sqlsrv.ini
-printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.1/mods-available/pdo_sqlsrv.ini
-exit
+sudo printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.1/mods-available/sqlsrv.ini
+sudo printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.1/mods-available/pdo_sqlsrv.ini
 sudo phpenmod -v 8.1 sqlsrv pdo_sqlsrv
 
 #Step 4. Install Apache and configure driver loading
-sudo su
-#apt-get install libapache2-mod-php8.1 apache2
-a2dismod mpm_event
-a2enmod mpm_prefork
-a2enmod php8.1
-exit
+sudo apt-get install libapache2-mod-php8.1 apache2
+sudo a2dismod mpm_event
+sudo a2enmod mpm_prefork
+sudo a2enmod php8.1
 
 #Step 5. Restart Apache and test the sample script
 sudo service apache2 restart
