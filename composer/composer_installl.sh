5c9ffeead769c01e4d558c29cef2f6b7824a0926
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-10-26 11:25:54 -0400

    Update composer_installl.sh

--- composer/composer_installl.sh
+++ composer/composer_installl.sh
@@ -1,7 +1,8 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install apache2  php libapache2-mod-php php8.1-mbstring php-intl php-mysql gh nano ssmtp git -y 
 sudo apt install php-cli php-json php-mbstring php-xml php-pcov php-xdebug -y
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
-composer update
