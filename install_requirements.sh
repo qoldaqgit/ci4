3ece4d4621d68dd66d555b0a6de36c0f5a92dcf8
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-10-26 11:40:03 -0400

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -6,7 +6,7 @@
 
 #install minimum requirements
 sudo apt update && sudo apt upgrade -y
-sudo apt install apache2  php zip unzip php-zip libapache2-mod-php php8.1-mbstring php-intl php-mysql gh nano ssmtp git -y 
+sudo apt install apache2  php zip unzip php-zip php-curl libapache2-mod-php php8.1-mbstring php-intl php-mysql gh nano ssmtp git -y 
 sudo apt install php-cli php-json php-mbstring php-xml php-pcov php-xdebug -y
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
