fc0b468d1628e1f73333eb9c752e6ac1bef01f79
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2024-10-26 12:28:01 -0400

    Update composer_installl.sh

--- composer/composer_installl.sh
+++ composer/composer_installl.sh
@@ -6,3 +6,4 @@ php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b3
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
+wget https://raw.githubusercontent.com/qoldaqgit/ci4/refs/heads/main/composer/createProject.sh
